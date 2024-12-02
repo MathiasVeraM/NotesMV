7d0f54e62aef1ca16f09a765e3b54408dd40e0b8
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-01 19:20:42 -0500

    Se actualizó el About y Note page con Binding y el .cs de Note para ir cargando archivos.

--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -6,8 +6,23 @@ public partial class NotePage : ContentPage
     public NotePage()
 	{
 		InitializeComponent();
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
@@ -23,4 +38,5 @@ public partial class NotePage : ContentPage
 
         TextEditor.Text = string.Empty;
     }
+
 }
